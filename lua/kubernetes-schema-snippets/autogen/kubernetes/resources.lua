67e1c24221c48227709615637b066842770121b2
KevinNitroG <KevinNitroG@users.noreply.github.com>
2025-07-29 11:03:47 +0000

    feat(autogen): update kubernetes

--- lua/kubernetes-schema-snippets/autogen/kubernetes/resources.lua
+++ lua/kubernetes-schema-snippets/autogen/kubernetes/resources.lua
@@ -1,7 +1,9 @@
 local ls = require("luasnip")
 local t = ls.text_node
+local i = ls.insert_node
 
 return {
+  i(nil, "resource"),
   t("affinity"),
   t("affinity-v1"),
   t("aggregationrule"),
--- lua/kubernetes-schema-snippets/autogen/kubernetes/versions.lua
+++ lua/kubernetes-schema-snippets/autogen/kubernetes/versions.lua
@@ -1,7 +1,9 @@
 local ls = require("luasnip")
 local t = ls.text_node
+local i = ls.insert_node
 
 return {
+  i(nil, "version"),
   t("master"),
   t("master-local"),
   t("master-standalone"),
