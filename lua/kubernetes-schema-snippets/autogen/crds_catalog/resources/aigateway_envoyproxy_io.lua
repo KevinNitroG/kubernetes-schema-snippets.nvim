1d664bbd34033a51073e47147052cce854396f37
KevinNitroG <KevinNitroG@users.noreply.github.com>
2026-06-03 04:12:08 +0000

    feat(autogen): update crds catalog

--- lua/kubernetes-schema-snippets/autogen/crds_catalog/resources/aigateway_envoyproxy_io.lua
+++ lua/kubernetes-schema-snippets/autogen/crds_catalog/resources/aigateway_envoyproxy_io.lua
@@ -5,10 +5,16 @@ local i = ls.insert_node
 return function()
   return {
     t("aigatewayroute_v1alpha1"),
+    t("aigatewayroute_v1beta1"),
     t("aiservicebackend_v1alpha1"),
+    t("aiservicebackend_v1beta1"),
     t("backendsecuritypolicy_v1alpha1"),
+    t("backendsecuritypolicy_v1beta1"),
     t("gatewayconfig_v1alpha1"),
+    t("gatewayconfig_v1beta1"),
     t("mcproute_v1alpha1"),
+    t("mcproute_v1beta1"),
+    t("quotapolicy_v1alpha1"),
     i(nil, "resource"),
   }
 end
