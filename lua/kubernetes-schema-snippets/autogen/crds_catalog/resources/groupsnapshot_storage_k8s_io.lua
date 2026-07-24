68872ce983ff5e6026b5b66a55ac8ebbe1be85e7
KevinNitroG <KevinNitroG@users.noreply.github.com>
2026-07-24 02:42:12 +0000

    feat(autogen): update crds catalog

--- lua/kubernetes-schema-snippets/autogen/crds_catalog/resources/groupsnapshot_storage_k8s_io.lua
+++ lua/kubernetes-schema-snippets/autogen/crds_catalog/resources/groupsnapshot_storage_k8s_io.lua
@@ -4,9 +4,18 @@ local i = ls.insert_node
 
 return function()
   return {
+    t("volumegroupsnapshot_v1"),
+    t("volumegroupsnapshot_v1alpha1"),
     t("volumegroupsnapshot_v1beta1"),
+    t("volumegroupsnapshot_v1beta2"),
+    t("volumegroupsnapshotclass_v1"),
+    t("volumegroupsnapshotclass_v1alpha1"),
     t("volumegroupsnapshotclass_v1beta1"),
+    t("volumegroupsnapshotclass_v1beta2"),
+    t("volumegroupsnapshotcontent_v1"),
+    t("volumegroupsnapshotcontent_v1alpha1"),
     t("volumegroupsnapshotcontent_v1beta1"),
+    t("volumegroupsnapshotcontent_v1beta2"),
     i(nil, "resource"),
   }
 end
